d0a1d2acbb25a6e1a10b4eab930abde0b70b51b5
Wexler763 <danylkarp@gmail.com>
2025-02-28 10:38:34 +0200

    66

---
+++ 66/main.go
@@ -0,0 +1,22 @@
+package main
+
+func plusOne(digits []int) []int {
+	return rec(digits, len(digits)-1)
+}
+
+func rec(digits []int, i int) []int {
+	if digits[i] != 9 {
+		digits[i] = digits[i] + 1
+		return digits
+	} else {
+
+		digits[i] = 0
+		if i == 0 {
+			digits = append([]int{1}, digits...)
+		} else {
+			return rec(digits, i-1)
+		}
+
+	}
+	return digits
+}
